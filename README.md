262860cb50aa21935a2d16624a3dd3c9dfa0ae59
hyunsungkim <hyunsungkim@postech.ac.kr>
2021-03-09 23:05:06 +0900

    Add install script. Move *.c *.h files into src/

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # EECE338_2020_2H_W12
 
 12th Weekly project of EECE338 class held in 2020 Fall
+4th Weekly project of EECE338/374 class held in 2021 Spring
+
+TA Hyunsung Kim (hyunsungkim@postech.ac.kr)
---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+#
+# Author: hyunsungkim (hyunsungkim@postech.ac.kr)
+# Last modified: 2021-03-09
+
+MODE=$1
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "ERROR: Run the script with sudo"
+    exit
+fi
+
+export python=python3
+export pip=pip3
+
+if [ $MODE == "--install" ]; then
+    #echo "install mode"
+    sudo apt update
+    sudo apt install python-setuptools python3-setuptools
+
+    cd /home/pi
+    wget https://github.com/joan2937/pigpio/archive/master.zip
+    unzip master.zip
+
+    cd pigpio-master
+    make
+    sudo make install
+
+if [ $MODE == '--debug' ]; then
+    #echo "debug mode"
+    cd /home/pi/pigpio-master
+
+    gcc --version
+    dpkg -l python*-setuptools
+
+    # inspect pigpio
+    sudo ./x_pigpio # check C I/F
+    sudo pigpiod    # start daemon
+    ./x_pigpiod_if2 # check C      I/F to daemon
+    ./x_pigpio.py   # check Python I/F to daemon
+    ./x_pigs        # check pigs   I/F to daemon
+    ./x_pipe        # check pipe   I/F to daemon
+
+    gpio readall
+
+#    python --version
+#    pip --version
+#    python debug.py
+
+else
+    echo "./install.sh --install"
+    echo "./install.sh --debug"
+fi
